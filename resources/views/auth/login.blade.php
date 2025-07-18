3f19758d76b13e6e1a351cb3f4d555c751fd8bab
AIVIBE11 <aidilridha11@gmail.com>
2025-07-18 10:40:23 +0700

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -41,11 +41,11 @@
                         @enderror
                   </div>
 
-                  <button type="submit" class="btn btn-primary w-100">Login</button>
+                  <!-- <button type="submit" class="btn btn-primary w-100">Login</button> -->
 
                   <div class="d-flex align-items-center justify-content-between mb-4">
                     <div class="form-check">
-                        <input type="checkbox" class="form-check-input" id="remember" name="remember" {{ old('remember') ? 'checked' : '' }}>
+                        <!-- <input type="checkbox" class="form-check-input" id="remember" name="remember" {{ old('remember') ? 'checked' : '' }}> -->
                       
                       <label class="form-check-label text-dark" for="flexCheckChecked">
                         Remeber this Device
@@ -53,7 +53,24 @@
                     </div>
                     <a class="text-primary fw-bold" href="./index.html">Forgot Password ?</a>
                   </div>
-                  <a href="./index.html" class="btn btn-primary w-100 py-8 fs-4 mb-4 rounded-2">Sign In</a>
+                  <a href="./home" class="btn btn-primary w-100 py-8 fs-4 mb-4 rounded-2">Sign In</a>
+
+                  @if (Route::has('password.request'))
+            <a class="btn btn-link" href="{{ route('password.request') }}">
+                Forgot Your Password?
+            </a>
+        @endif
+                   <!-- <button type="submit" class="btn btn-primary">Login</button>
+        @if (Route::has('password.request'))
+            <a class="btn btn-link" href="{{ route('password.request') }}">
+                Forgot Your Password?
+            </a>
+        @endif -->
+
+        <div class="mb-3 form-check">
+            <input type="checkbox" class="form-check-input" id="remember" name="remember" {{ old('remember') ? 'checked' : '' }}>
+            <label class="form-check-label" for="remember">Remember Me</label>
+        </div>
                   <div class="d-flex align-items-center justify-content-center">
                     <p class="fs-4 mb-0 fw-bold">New to Modernize?</p>
                     <a class="text-primary fw-bold ms-2" href="./authentication-register.html">Create an account</a>
